f93d6d0d39a09eafb347c3b541f6d3d2713bf2b5
DudLudNil64 <ludwig.gothbergmeiton@elev.ga.lbs.se>
2025-11-03 11:16:15 +0100

    way to little
    fix the player mov

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,23 +2,28 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public float PlayerSpeed;
+
     [SerializeField] KeyCode Up = KeyCode.W;
     [SerializeField] KeyCode Down = KeyCode.S;
     [SerializeField] KeyCode Right = KeyCode.D;
     [SerializeField] KeyCode Left = KeyCode.A;
     void Update()
     {
+        // Player Rotation To Mouse -Lud
         Vector3 Diffrence = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         Diffrence.Normalize();
         float RotaitionZ = Mathf.Atan2(Diffrence.y, Diffrence.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, RotaitionZ - 90);
 
+        // Player Movement -Lud
         Vector2 MovementVector = Vector2.zero;
-        if (Input.GetKeyDown(Up)) { MovementVector += Vector2.up; }
-        if (Input.GetKeyDown(Down)) { MovementVector += Vector2.down; }
-        if (Input.GetKeyDown(Right)) { MovementVector += Vector2.right; }
-        if (Input.GetKeyDown(Up)) { MovementVector += Vector2.left; }
+        if (Input.GetKey(Up)) { MovementVector += Vector2.up; }
+        if (Input.GetKey(Down)) { MovementVector += Vector2.down; }
+        if (Input.GetKey(Right)) { MovementVector += Vector2.right; }
+        if (Input.GetKey(Up)) { MovementVector += Vector2.left; }
 
+        MovementVector = MovementVector.normalized * (PlayerSpeed * Time.deltaTime);
         gameObject.transform.position += new Vector3(MovementVector.x, MovementVector.y, 0);
     }
 }
