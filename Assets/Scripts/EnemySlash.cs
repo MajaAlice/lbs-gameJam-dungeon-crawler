c134a981a8e7f30a24e7d2c0bc1be227e979c674
MajaAlice <filip.franas@elev.ga.lbs.se>
2025-11-06 13:27:22 +0100

    some work on the demo
    still no map

--- Assets/Scripts/EnemySlash.cs
+++ Assets/Scripts/EnemySlash.cs
@@ -5,7 +5,7 @@ public class EnemySlash : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(gameObject, 2f);
+        Destroy(gameObject, 0.4f);
     }
 
     // Update is called once per frame
